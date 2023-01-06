b39a0962a0c26b001da4d66418238ce3100e5d6b
Steve <104333881+StevesGitRepo@users.noreply.github.com>
2023-01-06 10:57:33 -0700

    Initial commit

---
+++ js/site.js
@@ -0,0 +1,20 @@
+// Call Hello World
+function helloWorld() {
+  alert('Hello World');
+}
+
+let first = 'Steve';
+let last = 'Szymanski';
+let msg = `Hello + ${first} ${last}`;
+
+console.log(msg);
+
+let city = 'Phoenix';
+let state = 'AZ';
+let location = `You are in ${city}, ${state}`;
+console.log(location);
+
+let name = 'Steve Szymanski';
+
+name.substring(6,9) // "Szym"
+name.substring(8,11) //"yman"
